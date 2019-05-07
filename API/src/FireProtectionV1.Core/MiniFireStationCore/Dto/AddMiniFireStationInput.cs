dd4a54645c521b2cc1ba69a6f70a4457b4c7dc47
guohai <18503806@qq.com>
2019-05-07 10:41:20 +0800

    修改微型消防站接口

--- API/src/FireProtectionV1.Core/MiniFireStationCore/Dto/AddMiniFireStationInput.cs
+++ API/src/FireProtectionV1.Core/MiniFireStationCore/Dto/AddMiniFireStationInput.cs
@@ -1,11 +1,47 @@
 ﻿using FireProtectionV1.MiniFireStationCore.Model;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace FireProtectionV1.MiniFireStationCore.Dto
 {
-    public class AddMiniFireStationInput : MiniFireStation
+    public class AddMiniFireStationInput
     {
+        /// <summary>
+        /// 站点名称
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+        /// <summary>
+        /// 站点等级
+        /// </summary>
+        public int Level { get; set; }
+        /// <summary>
+        /// 联系人
+        /// </summary>
+        public string Contact { get; set; }
+        /// <summary>
+        /// 联系电话
+        /// </summary>
+        public string ContactPhone { get; set; }
+        /// <summary>
+        /// 人员配备数量
+        /// </summary>
+        public int PersonNum { get; set; }
+        /// <summary>
+        /// 地址
+        /// </summary>
+        public string Address { get; set; }
+        /// <summary>
+        /// 经度
+        /// </summary>
+        [Required]
+        public decimal Lng { get; set; }
+        /// <summary>
+        /// 纬度
+        /// </summary>
+        [Required]
+        public decimal Lat { get; set; }
     }
 }
--- API/src/FireProtectionV1.Core/MiniFireStationCore/Manager/MiniFireStationManager.cs
+++ API/src/FireProtectionV1.Core/MiniFireStationCore/Manager/MiniFireStationManager.cs
@@ -3,6 +3,7 @@ using Abp.AutoMapper;
 using Abp.Domain.Repositories;
 using Abp.Domain.Services;
 using FireProtectionV1.Common.DBContext;
+using FireProtectionV1.Common.Helper;
 using FireProtectionV1.MiniFireStationCore.Dto;
 using FireProtectionV1.MiniFireStationCore.Model;
 using System;
@@ -15,11 +16,11 @@ namespace FireProtectionV1.MiniFireStationCore.Manager
 {
     public class MiniFireStationManager : DomainService, IMiniFireStationManager
     {
-        IRepository<MiniFireStation> _MiniFireStationRepository;
+        IRepository<MiniFireStation> _miniFireStationRepository;
 
         public MiniFireStationManager(IRepository<MiniFireStation> miniFireStationRepository)
         {
-            _MiniFireStationRepository = miniFireStationRepository;
+            _miniFireStationRepository = miniFireStationRepository;
         }
 
         /// <summary>
@@ -29,8 +30,30 @@ namespace FireProtectionV1.MiniFireStationCore.Manager
         /// <returns></returns>
         public async Task<int> Add(AddMiniFireStationInput input)
         {
+            Valid.Exception(_miniFireStationRepository.Count(m => input.Name.Equals(m.Name)) > 0, "保存失败：站点名称已存在");
+
             var entity = input.MapTo<MiniFireStation>();
-            return await _MiniFireStationRepository.InsertAndGetIdAsync(entity);
+            return await _miniFireStationRepository.InsertAndGetIdAsync(entity);
+        }
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task Delete(int id)
+        {
+            await _miniFireStationRepository.DeleteAsync(id);
+        }
+
+        /// <summary>
+        /// 获取单个微型消防站信息
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<MiniFireStation> GetById(int id)
+        {
+            return await _miniFireStationRepository.GetAsync(id);
         }
 
         /// <summary>
@@ -40,7 +63,7 @@ namespace FireProtectionV1.MiniFireStationCore.Manager
         /// <returns></returns>
         public Task<PagedResultDto<MiniFireStation>> GetList(GetMiniFireStationListInput input)
         {
-            var miniFireStations = _MiniFireStationRepository.GetAll();
+            var miniFireStations = _miniFireStationRepository.GetAll();
 
             var expr = ExprExtension.True<MiniFireStation>()
              .IfAnd(!string.IsNullOrEmpty(input.Name), item => input.Name.Contains(item.Name));
@@ -54,6 +77,19 @@ namespace FireProtectionV1.MiniFireStationCore.Manager
             var tCount = miniFireStations.Count();
 
             return Task.FromResult(new PagedResultDto<MiniFireStation>(tCount, list));
+        }
+
+        /// <summary>
+        /// 修改
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public async Task Update(UpdateMiniFireStationInput input)
+        {
+            Valid.Exception(_miniFireStationRepository.Count(m => input.Name.Equals(m.Name) && !input.Id.Equals(m.Id)) > 0, "保存失败：站点名称已存在");
+
+            var entity = input.MapTo<MiniFireStation>();
+            await _miniFireStationRepository.UpdateAsync(entity);
         }
     }
 }
