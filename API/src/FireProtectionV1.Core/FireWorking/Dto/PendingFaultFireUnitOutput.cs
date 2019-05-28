1460a4333ae8d5f5c7e74ea346db55e6626f9c1d
chenlong <18113190102@189.cn>
2019-05-28 11:19:09 +0800

    添加故障接口

---
+++ API/src/FireProtectionV1.Core/FireWorking/Dto/PendingFaultFireUnitOutput.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FireProtectionV1.FireWorking.Dto
+{
+    /// <summary>
+    /// 待处理故障单位
+    /// </summary>
+    public class PendingFaultFireUnitOutput
+    {
+        /// <summary>
+        /// 防火单位Id
+        /// </summary>
+        public int FireUnitId { get; set; }
+        /// <summary>
+        /// 防火单位名称
+        /// </summary>
+        public string FireUnitName { get; set; }
+        /// <summary>
+        /// 故障数量
+        /// </summary>
+        public int Count { get; set; }
+        /// <summary>
+        /// 待处理故障数量
+        /// </summary>
+        public int PendingCount { get; set; }
+    }
+}
---
+++ API/src/FireProtectionV1.Core/FireWorking/Manager/IFaultManager.cs
@@ -0,0 +1,17 @@
+﻿using Abp.Domain.Services;
+using FireProtectionV1.FireWorking.Dto;
+using FireProtectionV1.FireWorking.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FireProtectionV1.FireWorking.Manager
+{
+    public interface IFaultManager : IDomainService
+    {
+        IQueryable<Fault> GetFaultDataAll();
+        IQueryable<Fault> GetFaultDataMonth(int year, int month);
+        IQueryable<PendingFaultFireUnitOutput> GetPendingFaultFireUnits();
+    }
+}
