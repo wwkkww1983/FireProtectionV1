97f72b09e0822aaba3f4656950ab796b14b9517e
guohai <18503806@qq.com>
2019-08-12 09:15:36 +0800

    删除前端代码

--- WebVue/src/plugins/BaseComponent.js
+++
@@ -1,6 +0,0 @@
-// todo 全局注册基础组件
-import Vue from "vue";
-import BaseDialog from "../components/BaseDialog";
-
-// 弹窗
-Vue.component("BaseDialog", BaseDialog);
--- WebVue/src/plugins/element.js
+++
@@ -1,5 +0,0 @@
-import Vue from "vue";
-import Element from "element-ui";
-import "../style/element-variables.scss";
-
-Vue.use(Element);
