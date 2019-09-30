c2790b52c187e1d5caf65105b2e97613dff57bc4
chenlong <18113190102@189.cn>
2019-09-30 11:54:56 +0800

    添加接口缩略图的字段，添加大联动接口的调用

---
+++ API/GovFire/Dto/AlarmDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GovFire.Dto
+{
+    public class AlarmDto
+    {
+        public string firecompany { get; set; }
+        public string devicesn { get; set; }
+        public string devicetype { get; set; }
+        public string devicelocation { get; set; }
+        public string alarmtime { get; set; }
+        public string additionalinfo { get; set; }
+        /// <summary>
+        /// 维度
+        /// </summary>
+        public string lat { get; set; }
+        /// <summary>
+        /// 经度
+        /// </summary>
+        public string lon { get; set; }
+    }
+}
---
+++ API/GovFire/Dto/EventDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GovFire.Dto
+{
+    public class EventDto
+    {
+        public string id { get; set; }
+        public string firecompany { get; set; }
+        public string eventtype { get; set; }
+        public string eventcontent { get; set; }
+        public string createtime { get; set; }
+        public string donetime { get; set; }
+        public string state { get; set; }
+        /// <summary>
+        /// 维度
+        /// </summary>
+        public string lat { get; set; }
+        /// <summary>
+        /// 经度
+        /// </summary>
+        public string lon { get; set; }
+        /// <summary>
+        /// 大联动数据Id
+        /// </summary>
+        public string fireUnitId { get; set; }
+    }
+}
