c84bfcd59a598baac236e84b7f518eb79b3bdf8b
guohai <18503806@qq.com>
2019-12-13 10:06:44 +0800

    更新数据库

---
+++ API/src/FireProtectionV1.EntityFrameworkCore/Migrations/20191213020554_20191213.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FireProtectionV1.Migrations
+{
+    public partial class _20191213 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GatewayId",
+                table: "FireWaterDevice",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "FireWaterDeviceType",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    CreationTime = table.Column<DateTime>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    Name = table.Column<string>(maxLength: 20, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FireWaterDeviceType", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FireWaterDeviceType");
+
+            migrationBuilder.DropColumn(
+                name: "GatewayId",
+                table: "FireWaterDevice");
+        }
+    }
+}
