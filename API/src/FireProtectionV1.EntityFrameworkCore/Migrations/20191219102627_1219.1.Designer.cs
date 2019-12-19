32faf0c57ca20bb56fe335e992b6dc69966c7610
chenlong <18113190102@189.cn>
2019-12-19 18:27:14 +0800

    完善数据库

--- API/src/FireProtectionV1.EntityFrameworkCore/Migrations/20191219102627_1219.1.Designer.cs
+++ API/src/FireProtectionV1.EntityFrameworkCore/Migrations/20191219102627_1219.1.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using FireProtectionV1.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FireProtectionV1.Migrations
 {
     [DbContext(typeof(FireProtectionV1DbContext))]
-    partial class FireProtectionV1DbContextModelSnapshot : ModelSnapshot
+    [Migration("20191219102627_1219.1")]
+    partial class _12191
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ API/src/FireProtectionV1.EntityFrameworkCore/Migrations/20191219102627_1219.1.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FireProtectionV1.Migrations
+{
+    public partial class _12191 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Lng",
+                table: "MiniFireStation",
+                type: "decimal(9,6)",
+                nullable: false,
+                oldClrType: typeof(decimal));
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Lat",
+                table: "MiniFireStation",
+                type: "decimal(9,6)",
+                nullable: false,
+                oldClrType: typeof(decimal));
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Lng",
+                table: "Hydrant",
+                type: "decimal(9,6)",
+                nullable: false,
+                oldClrType: typeof(decimal));
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Lat",
+                table: "Hydrant",
+                type: "decimal(9,6)",
+                nullable: false,
+                oldClrType: typeof(decimal));
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Lng",
+                table: "FireUnit",
+                type: "decimal(9,6)",
+                nullable: false,
+                oldClrType: typeof(decimal));
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Lat",
+                table: "FireUnit",
+                type: "decimal(9,6)",
+                nullable: false,
+                oldClrType: typeof(decimal));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Lng",
+                table: "MiniFireStation",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(9,6)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Lat",
+                table: "MiniFireStation",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(9,6)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Lng",
+                table: "Hydrant",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(9,6)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Lat",
+                table: "Hydrant",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(9,6)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Lng",
+                table: "FireUnit",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(9,6)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Lat",
+                table: "FireUnit",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(9,6)");
+        }
+    }
+}
