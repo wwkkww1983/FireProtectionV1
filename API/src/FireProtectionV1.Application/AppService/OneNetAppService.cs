0cf87840f607484fa9a5e663edd2db01de74d851
chenlong <18113190102@189.cn>
2019-12-24 11:51:08 +0800

    添加OneNet测试接口

---
+++ API/src/FireProtectionV1.Application/AppService/OneNetAppService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireProtectionV1.AppService
+{
+    public class OneNetAppService: AppServiceBase
+    {
+        /// <summary>
+        /// OneNet测试
+        /// </summary>
+        /// <param name="nonce"></param>
+        /// <param name="msg"></param>
+        /// <param name="signature"></param>
+        /// <returns></returns>
+        public async Task<string> Test(string nonce, string msg, string signature)
+        {
+            return msg;
+        }
+    }
+}
