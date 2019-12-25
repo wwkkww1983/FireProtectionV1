ad98a2d20afda09ea5816552800e26c1d07b2557
chenlong <18113190102@189.cn>
2019-12-25 09:23:13 +0800

    完善OneNet对接接口

--- API/src/FireProtectionV1.Application/AppService/OneNetAppService.cs
+++ API/src/FireProtectionV1.Application/AppService/OneNetAppService.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using Abp.Web.Models;
+using FireProtectionV1.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FireProtectionV1.AppService
 {
-    public class OneNetAppService: AppServiceBase
+    public class OneNetAppService: HttpContextAppService
     {
+        public OneNetAppService(IHttpContextAccessor httpContext) : base(httpContext)
+        {
+
+        }
         /// <summary>
         /// OneNet测试
         /// </summary>
@@ -14,9 +24,38 @@ namespace FireProtectionV1.AppService
         /// <param name="msg"></param>
         /// <param name="signature"></param>
         /// <returns></returns>
+        [DontWrapResult]
+        [HttpGet]
         public async Task<string> Test(string nonce, string msg, string signature)
         {
-            return msg;
+            var v = _httpContext.HttpContext.Request.Body;
+            using(var rd=new StreamReader(v))
+            {
+                var json = await rd.ReadToEndAsync();
+                var input=JsonConvert.DeserializeObject<OneNetInput>(json);
+            }
+            Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} nonce={nonce} msg={msg} signature={signature}");
+            return msg==null?"":msg;
         }
+        [DontWrapResult]
+        public async Task Test(OneNetInput input)
+        {
+            try
+            {
+                Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} {JsonConvert.SerializeObject(input)}");
+            }catch(Exception e)
+            {
+
+            }
+        }
+        //public async Task TestPost()
+        //{
+        //    var v = _httpContext.HttpContext.Request.Body;
+        //    using (var rd = new StreamReader(v))
+        //    {
+        //        var json = await rd.ReadToEndAsync();
+        //        var input = JsonConvert.DeserializeObject<OneNetInput>(json);
+        //    }
+        //}
     }
 }
---
+++ API/src/FireProtectionV1.Application/Dto/OneNetInput.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FireProtectionV1.Dto
+{
+    public class OneNetInput
+    {
+        public int type { get; set; }
+        public int dev_id { get; set; }
+        public string ds_id { get; set; }
+        public int at { get; set; }
+        public string value { get; set; }
+        public int status { get; set; }
+        public int login_type { get; set; }
+        public int cmd_type { get; set; }
+        public string cmd_id { get; set; }
+        public string msg_signature { get; set; }
+        public string nonce { get; set; }
+        public string enc_msg { get; set; }
+    }
+}
